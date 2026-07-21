a65fbd7b7ec14decaa7d9e502df96f1edcd3c654
Ashok <Ashok@gmail.com>
2026-07-21 22:48:31 +0530

    Add Flask application Ans Dockerfile

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Cloud & Devops production Platfrom Docker Deployment"
+
+if __name__ == "__main ":
+    app.run(host="0.0.0.0", port=5000)
