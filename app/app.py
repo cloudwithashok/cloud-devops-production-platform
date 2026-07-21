1a647ecea4771d3ab04316fa17e867eae7405e03
Ashok <Ashok@gmail.com>
2026-07-21 23:15:26 +0530

    Fix Flask Application

--- app/app.py
+++ app/app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def home():
     return "Cloud & Devops production Platfrom Docker Deployment"
 
-if __name__ == "__main ":
+if __name__ == "__main":
     app.run(host="0.0.0.0", port=5000)
