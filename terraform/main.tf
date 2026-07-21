ca074a725b73aa8384285296fe78b06283436c55
Ashok <Ashok@gmail.com>
2026-07-21 22:47:05 +0530

    Complete Terraform infastructure With outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "aws_security_group" "web_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "1"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "instance_id" {
+    description = "EC2 Instance_ID"
+    value = aws_instance.production_server.id
+
+  
+}
+output "public_ip" {
+    description = "EC2 Public IP"
+    value = aws_instance.production_server.public_ip
+}
+
+output "public_dns" {
+    description = "EC2 Public DNS"
+    value = aws_instance.production_server.public_dns
+  
+}
